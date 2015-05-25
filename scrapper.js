abdfa3fb4d8e8c4af9fb7148453c06181faa6237
cs12b015 <jayanthk@cse.iitm.ac.in>
2015-05-25 14:16:46 +0530

    problem wit page number

--- scrapper.js
+++ scrapper.js
@@ -16,10 +16,29 @@ request(MAINURL, function (error, response, body)
 	  	$('.row.tal.mbl.limit-width-1200').find('a').each(function(index,item) 
 	  	{
 		  	var boss= $(item).attr('href');
-		  	links[index]=boss+"schools/?page=";
-		  	console.log(links[index]);
+		  	links[index]=boss+"schools/?page=1";
+		  	//console.log(links[index]);
 		});
 		console.log("got links");
-		
+		dothething(links);
 	}
 });
+
+var dothething= function(links){
+	var linksize = links.length;
+	// for(var i=0;i<linksize;i++){
+	for(var i=0;i<1;i++){
+		var lastpagenumb;
+		request(links[i],function(error, response, body){
+			if (!error && response.statusCode == 200) 
+		  	{
+			  	$ = cheerio.load(body);
+			  	var list = $('.pagination.page.js-no_ad a');
+			  	//var temp = $(list[list.length-1]);
+			  	var temp1 = $(list.find('a').text());
+			  	// console.log(temp);
+			  	console.log(temp1);
+			}
+		});
+	}
+}
