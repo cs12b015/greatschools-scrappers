70501ffccae80983a8dcbbc6802be4b5ce32d366
cs12b015 <jayanthk@cse.iitm.ac.in>
2015-05-25 13:02:00 +0530

    Links working now

--- scrapper.js
+++ scrapper.js
@@ -7,12 +7,19 @@ var cheerio = require('cheerio');
 var MAINURL="http://www.greatschools.org/";
 
 
-request(MAINURL, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-  	$ = cheerio.load(body);
-
-  	var boss= $('.row.tal.mbl.limit-width-1200').find('a').attr('href');
-  	console.log(boss);
-    //console.log(body);
-  }
+request(MAINURL, function (error, response, body) 
+{
+  	if (!error && response.statusCode == 200) 
+  	{
+  		var links =[];
+	  	$ = cheerio.load(body);
+	  	$('.row.tal.mbl.limit-width-1200').find('a').each(function(index,item) 
+	  	{
+		  	var boss= $(item).attr('href');
+		  	links[index]=boss+"schools/?page=";
+		  	console.log(links[index]);
+		});
+		console.log("got links");
+		
+	}
 });
