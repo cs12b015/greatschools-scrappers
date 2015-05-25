7dcc33e5ed00e0b2e35cbfd6b00ff080cba0ab67
cs12b015 <jayanthk@cse.iitm.ac.in>
2015-05-25 12:12:34 +0530

    trying for urls

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # greatschools-scrappers
 srappers for the greatschools.org
+
+
+//commands to run the code
+
+npm install
+
+node scrapper.js
--- scrapper.js
+++ scrapper.js
@@ -15,31 +15,4 @@ request(MAINURL, function (error, response, body) {
   	console.log(boss);
     //console.log(body);
   }
-})
-
-
-
-
-
-
-
-/*var getStatesUrl=function(url){
-	console.log('fetching from ' + url);
-	request(url, function(error, res, body) {
-		if(error) console.error(error); //cb(err);
-		var $ = cheerio.load(body, {
-			normalizeWhitespace: true,
-			xmlMode: false,
-			decodeEntities: true
-		});
-		console.log(body);
-		//getDataUrl($, function(err, data){
-			//if(err) cb(err);
-			// appendToFile(data);
-		
-		//});
-	});
-};
-getStatesUrl(MAINURL,function(err){
-//console.log(data);
-});*/
+});
