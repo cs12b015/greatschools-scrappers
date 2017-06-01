c92ef1bd0df4656c636fe4f390ab13722aa998de
jayanth <cs12b015@smail.iitm.ac.in>
2017-06-01 17:39:14 +0530

    throttling requests

--- scrapper.js
+++ scrapper.js
@@ -9,6 +9,8 @@ var async = require('async');
 var request = require('request');
 var cheerio = require('cheerio');
 
+var mainnumbproc = parseInt(process.argv[2])
+
 var MAINURL="http://www.greatschools.org/schools/cities/Alabama/AL/";
 var links=[];
 
@@ -29,7 +31,6 @@ request(MAINURL, function (error, response, body)
 
 //count the page numbers and calls the urls
 var dothething= function(links){
-	//console.log(links)
 	var linksize = links.length;
 	for(var i=0;i<linksize;i++){
 		var pasturl=links[i];
@@ -37,17 +38,25 @@ var dothething= function(links){
 	}
 };
 
+var setFetch =function(url,time){
+	setTimeout(function(){
+		fetch(url);
+	}, time)
+}
+
+
 var fetch_city = function(reqUrl){
 	request(reqUrl, function (error, response, body)
 	{
 	  	if (!error && response.statusCode == 200)
 	  	{
 		  	$ = cheerio.load(body);
-		  	$(".city-district-link a").each(function()
-			{
-				var city_link = $(this).attr("href")+"schools/?page=";
-				fetch(city_link)
-			});
+		  	var arr =$(".city-district-link a");
+		  	for(var i=0;i<arr.length;i++){
+		  		var feturl = arr[i].attribs.href+"schools/?page=";
+		  		var timeouttime =2*i*1000;
+		  		setFetch(feturl,timeouttime);
+		  	}
 		}
 	});
 }
@@ -85,7 +94,7 @@ var fetch = function(pasturl){
 							filename=filnamarr[3]+"-"+filnamarr[4];
 							statedata=statedata.concat(data);
 							var pagnunmb=filnamarr[filnamarr.length-1].split('?');
-							console.log("Writhing the "+filename+" "+pagnunmb[1]+" to "+filename+".csv file");
+							console.log("Writing the "+filename+" "+pagnunmb[1]+" to "+filename+".csv file");
 							writeToFile(filnamarr[3],filename,data);
 						}
 					});
@@ -96,7 +105,7 @@ var fetch = function(pasturl){
 				var filnamarr2=pasturl.split('/');
 				var filename2=filnamarr2[3]+"-"+filnamarr2[4];
 				writeToFile(filnamarr2[3],filename2,"name,address,review,district,grade\n");
-				GetDetails(currenturl,function(err,data,pasturl){
+				GetExistingDetails(currenturl,body,function(err,data,pasturl){
 					if(err)
 						throw err;
 					else
@@ -105,7 +114,7 @@ var fetch = function(pasturl){
 						filename=filnamarr[3]+"-"+filnamarr[4];
 						statedata=statedata.concat(data);
 						var pagnunmb=filnamarr[filnamarr.length-1].split('?');
-						console.log("Writhing the "+filename+" "+pagnunmb[1]+" to "+filename+".csv file");
+						console.log("Writing the "+filename+" "+pagnunmb[1]+" to "+filename+".csv file");
 						writeToFile(filnamarr[3],filename,data);
 					}
 				});
@@ -113,6 +122,34 @@ var fetch = function(pasturl){
 			}
 		}
 	});
+};
+
+var GetExistingDetails = function(url,body,cb){
+	
+		var school = [];
+
+  	$ = cheerio.load(body);
+  	$('.js-schoolSearchResult.js-schoolSearchResultCompareErrorMessage').each(function(index, item) {
+  		var schoolData = {};
+  		var name=$(item).find('.open-sans_sb.mbs.font-size-medium.rs-schoolName').text();
+  		var address=$(item).find('.hidden-xs.font-size-small.rs-schoolAddress').text();
+  		var tempreview=$(item).find('.font-size-small.js-reviewCount').text();
+  		var revarr=tempreview.split(" ");
+  		var reviewnumb=revarr[0];
+  		var district=$(item).find('.font-size-small.mvm.clearfix.ptm.hidden-xs .prs.fl').text();
+  		var grade=$(item).find('.font-size-small.mvm.clearfix.ptm.hidden-xs .fl:nth-child(4)').text()
+
+  		schoolData = {
+  			name: name,
+  			address: address,
+  			review: reviewnumb,
+  			district: district,
+  			grade: grade
+  		};
+  		school.push(schoolData);
+  	});
+  	cb(null,school,url);
+
 };
 
 
